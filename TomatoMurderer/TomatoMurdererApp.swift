89aac484f68849f4757c20e49be6ef01f7aad8ea
danzelmba15 <notwatermango@gmail.com>
2024-04-26 14:23:21 +0700

    Initial Commit

---
+++ TomatoMurderer/TomatoMurdererApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoMurdererApp.swift
+//  TomatoMurderer
+//
+//  Created by mg0 on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TomatoMurdererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
